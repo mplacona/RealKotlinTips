77c89d6acc7c6b0acbcb08f37da079025bb2efa3
mplacona <marcos.placona@gmail.com>
2018-04-15 19:21:18 +0100

    :sparkles: Add new post for Defining a Map

---
+++ src/main/kotlin/Examples/DefiningAMap.kt
@@ -0,0 +1,20 @@
+package Examples
+
+/**
+ * Name: Defining a map in Kotlin
+ *
+ * Intro: No need for explicitly defining generic types and no repetitive `put` or assignment to define maps.
+ * Outro: From time to time, there is a need to define a hardcoded `Map`. There are several ways to do them in Java, but nothing gets closer to the beauty of Kotlin.
+ */
+
+fun createImplicitMap(): Map<String, String>{
+    return mapOf(
+            "keyA" to "valueA",
+            "keyB" to "valueB",
+            "keyC" to "valueC"
+    )
+}
+
+fun main(args: Array<String>) {
+    println(createImplicitMap())
+}
--- src/test/kotlin/DefiningAMapSpek.kt
+++ src/test/kotlin/DefiningAMapSpek.kt
@@ -1,2 +1,21 @@
-class DefiningAMapSpek {
-}
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.junit.jupiter.api.Assertions
+
+class DefiningAMapSpek: Spek({
+    context("Defining a map in Kotlin"){
+        describe("Defining a map without types or the use of put") {
+            it("should return a map that has the same structure as one manually defined") {
+                val expected = HashMap<String, String>()
+                expected["keyA"] = "valueA"
+                expected["keyB"] = "valueB"
+                expected["keyC"] = "valueC"
+
+                val actual = Examples.createImplicitMap()
+                Assertions.assertEquals(expected, actual)
+            }
+        }
+    }
+})
