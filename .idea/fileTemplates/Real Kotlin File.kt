ea6de9ac593023fd4b437da9732b0e667eda9e39
mplacona <marcos.placona@gmail.com>
2018-04-15 19:51:18 +0100

    :memo: Remove templates from ignore and add them in

---
+++ .idea/fileTemplates/Real Kotlin File.kt
@@ -0,0 +1,17 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
+
+#end
+#parse("File Header.java")
+
+/**
+ * Name: 
+ *
+ * Intro: 
+ * Outro: 
+ */
+
+
+ 
+fun main(args: Array<String>) {
+    
+}
---
+++ .idea/fileTemplates/Real Kotlin Spek.kt
@@ -0,0 +1,28 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
+
+#end
+#parse("File Header.java")
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.junit.jupiter.api.Assertions
+
+class ${NAME}: Spek({
+    context("${NAME}"){
+        describe("${DESCRIPTION}") {
+            it("${BEHAVIOUS}") {
+                val expected = HashMap<String, String>()
+                val actual = Examples.${METHOD_NAME}
+                Assertions.assertEquals(expected, actual)
+            }
+        }
+    }
+})
+
+
+ 
+fun main(args: Array<String>) {
+    
+}
